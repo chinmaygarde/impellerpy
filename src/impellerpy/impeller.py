c5588bbfa60b946bcf163b2e0d2c75aff4293807
Chinmay Garde <chinmaygarde@gmail.com>
2025-05-03 16:58:57 -0700

    PR

--- src/impellerpy/impeller.py
+++ src/impellerpy/impeller.py
@@ -1,7 +1,7 @@
 from typing import List
-from impellerpy import impellerpy
+from .impellerpy import *
 
-class Rect(impellerpy.Rect_):
+class Rect(Rect_):
     def __init__(
         self,
         x: float = 0,
@@ -16,7 +16,7 @@ class Rect(impellerpy.Rect_):
         self.height = height
 
 
-class Point(impellerpy.Point_):
+class Point(Point_):
     def __init__(
         self,
         x: float = 0,
@@ -27,28 +27,28 @@ class Point(impellerpy.Point_):
         self.y = y
 
 
-class Size(impellerpy.Size_):
+class Size(Size_):
     def __init__(self, width: float = 0, height: float = 0):
         super().__init__()
         self.width = width
         self.height = height
 
 
-class ISize(impellerpy.ISize_):
+class ISize(ISize_):
     def __init__(self, width: int = 0, height: int = 0):
         super().__init__()
         self.width = width
         self.height = height
 
 
-class Range(impellerpy.Range_):
+class Range(Range_):
     def __init__(self, start: int = 0, end: int = 0):
         super().__init__()
         self.start = start
         self.end = end
 
 
-class Matrix(impellerpy.Matrix_):
+class Matrix(Matrix_):
     def __init__(self, row_col_list: List[List[float]]):
         values = []
         for col in range(4):
@@ -57,15 +57,15 @@ class Matrix(impellerpy.Matrix_):
         super().__init__(values)
 
 
-class ColorMatrix(impellerpy.ColorMatrix_):
+class ColorMatrix(ColorMatrix_):
     pass
 
 
-class RoundingRadii(impellerpy.RoundingRadii_):
+class RoundingRadii(RoundingRadii_):
     pass
 
 
-class Color(impellerpy.Color_):
+class Color(Color_):
     def __init__(
         self,
         r: float = 0.0,
@@ -80,10 +80,10 @@ class Color(impellerpy.Color_):
         self.alpha = a
 
 
-class TextureDescriptor(impellerpy.TextureDescriptor_):
+class TextureDescriptor(TextureDescriptor_):
     def __init__(
         self,
-        pixel_format: impellerpy.PixelFormat,
+        pixel_format: PixelFormat,
         texture_size: ISize,
         mip_count: int = 1,
     ):
@@ -93,78 +93,78 @@ class TextureDescriptor(impellerpy.TextureDescriptor_):
         self.mip_count = mip_count
 
 
-class ColorFilter(impellerpy.ColorFilter_):
+class ColorFilter(ColorFilter_):
     pass
 
 
-class ColorSource(impellerpy.ColorSource_):
+class ColorSource(ColorSource_):
     pass
 
 
-class ImageFilter(impellerpy.ImageFilter_):
+class ImageFilter(ImageFilter_):
     pass
 
 
-class MaskFilter(impellerpy.MaskFilter_):
+class MaskFilter(MaskFilter_):
     pass
 
 
-class Paint(impellerpy.Paint_):
+class Paint(Paint_):
     pass
 
 
-class DisplayList(impellerpy.DisplayList_):
+class DisplayList(DisplayList_):
     pass
 
 
-class Path(impellerpy.Path_):
+class Path(Path_):
     pass
 
 
-class LineMetrics(impellerpy.LineMetrics_):
+class LineMetrics(LineMetrics_):
     pass
 
 
-class GlyphInfo(impellerpy.GlyphInfo_):
+class GlyphInfo(GlyphInfo_):
     pass
 
 
-class Paragraph(impellerpy.Paragraph_):
+class Paragraph(Paragraph_):
     pass
 
 
-class Texture(impellerpy.Texture_):
+class Texture(Texture_):
     pass
 
 
-class DisplayListBuilder(impellerpy.DisplayListBuilder_):
+class DisplayListBuilder(DisplayListBuilder_):
     pass
 
 
-class PathBuilder(impellerpy.PathBuilder_):
+class PathBuilder(PathBuilder_):
     pass
 
 
-class Surface(impellerpy.Surface_):
+class Surface(Surface_):
     pass
 
 
-class Context(impellerpy.Context_):
+class Context(Context_):
     pass
 
 
-class Window(impellerpy.Window_):
+class Window(Window_):
     def __init__(self):
         super().__init__()
 
 
-class TypographyContext(impellerpy.TypographyContext_):
+class TypographyContext(TypographyContext_):
     pass
 
 
-class ParagraphStyle(impellerpy.ParagraphStyle_):
+class ParagraphStyle(ParagraphStyle_):
     pass
 
 
-class ParagraphBuilder(impellerpy.ParagraphBuilder_):
+class ParagraphBuilder(ParagraphBuilder_):
     pass
